b81d56a578b3e17e67927073bd63e9622b739ef3
savioferraz <savio.ferraz88@gmail.com>
2023-04-24 02:04:27 -0300

    test: initial test suit

---
+++ tests/schemas/usersSchema.test.ts
@@ -0,0 +1,60 @@
+import { createAccountSchema } from "@/schemas/userSchemas";
+import { faker } from "@faker-js/faker";
+
+describe("createUserSchema", () => {
+  const generateValidInput = () => ({
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(6),
+  });
+
+  describe("when name is not valid", () => {
+    it("should return error if name is not present", () => {
+      const input = generateValidInput();
+      delete input.name;
+
+      const { error } = createAccountSchema.validate(input);
+
+      expect(error).toBeDefined();
+    });
+  });
+
+  describe("when email is not valid", () => {
+    it("should return error if email is not present", () => {
+      const input = generateValidInput();
+      delete input.email;
+
+      const { error } = createAccountSchema.validate(input);
+
+      expect(error).toBeDefined();
+    });
+
+    it("should return error if email does not follow valid email format", () => {
+      const input = generateValidInput();
+      input.email = faker.lorem.word();
+
+      const { error } = createAccountSchema.validate(input);
+
+      expect(error).toBeDefined();
+    });
+  });
+
+  describe("when password is not valid", () => {
+    it("should return error if password is not present", () => {
+      const input = generateValidInput();
+      delete input.password;
+
+      const { error } = createAccountSchema.validate(input);
+
+      expect(error).toBeDefined();
+    });
+  });
+
+  it("should return no error if input is valid", () => {
+    const input = generateValidInput();
+
+    const { error } = createAccountSchema.validate(input);
+
+    expect(error).toBeUndefined();
+  });
+});
